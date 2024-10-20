0209f26ecd5290d227be92c060aceb8d0a48eac4
lunarmaniac <egoism@riseup.net>
2024-10-20 22:23:35 +0100

    updated

--- src/me/egomaniac/kitpvp/commands/misc/RepairCommand.java
+++ src/me/egomaniac/kitpvp/commands/misc/RepairCommand.java
@@ -4,6 +4,7 @@ import me.egomaniac.kitpvp.Main;
 import me.egomaniac.kitpvp.utils.CC;
 import me.egomaniac.kitpvp.utils.commandFramework.CommandArgs;
 import me.egomaniac.kitpvp.utils.commandFramework.Command;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -13,16 +14,46 @@ public class RepairCommand {
     public RepairCommand() {
         Main.getInstance().framework.registerCommands(this);
     }
-    @Command(name = "repair", permission = "kitpvp.admin", inGameOnly = true)
-    public void execute(CommandArgs args) {
-        Player player = args.getPlayer();
 
-        for (ItemStack item : player.getInventory().getContents()) {
+
+    @Command(name = "repair", permission = "kitpvp.legend", inGameOnly = false)
+    public void execute(CommandArgs args) {
+        Player target;
+
+        if (args.length() == 1) {
+            if (!args.getSender().hasPermission("kitpvp.admin")) {
+                args.getSender().sendMessage(CC.translate("&cYou do not have permission to repair someone else's items."));
+                return;
+            }
+
+            target = Bukkit.getPlayer(args.getArgs(0));
+
+            if (target == null) {
+                args.getSender().sendMessage(CC.translate("&cPlayer not found or not online."));
+                return;
+            }
+        } else if (args.isPlayer()) {
+            target = args.getPlayer();
+
+            if (!target.hasPermission("kitpvp.legend")) {
+                target.sendMessage(CC.translate("&cYou do not have permission to repair your own items."));
+                return;
+            }
+        } else {
+            args.getSender().sendMessage(CC.translate("&cUsage: /repair <player>"));
+            return;
+        }
+
+        for (ItemStack item : target.getInventory().getContents()) {
             if (item != null && item.getType() != Material.AIR) {
                 item.setDurability((short) 0);
             }
         }
 
-        player.sendMessage(CC.translate("&aAll items in your inventory have been repaired."));
+        target.sendMessage(CC.translate("&aAll items in your inventory have been repaired."));
+
+        if (!target.equals(args.getSender())) {
+            args.getSender().sendMessage(CC.translate("&aYou have repaired " + target.getName() + "'s inventory."));
+        }
     }
 }
