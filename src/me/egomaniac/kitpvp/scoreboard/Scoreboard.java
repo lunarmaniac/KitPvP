2fd16fd48645b00e2905c2b0c00304eef05cce7c
lunarmaniac <egoism@riseup.net>
2024-10-20 22:23:52 +0100

    clean up + add cooldowns

--- src/me/egomaniac/kitpvp/scoreboard/Scoreboard.java
+++ src/me/egomaniac/kitpvp/scoreboard/Scoreboard.java
@@ -1,8 +1,10 @@
 package me.egomaniac.kitpvp.scoreboard;
 
+import me.egomaniac.kitpvp.managers.CooldownManager;
 import me.egomaniac.kitpvp.utils.CC;
 import me.egomaniac.kitpvp.Main;
 import me.egomaniac.kitpvp.scoreboard.api.AssembleAdapter;
+import me.egomaniac.kitpvp.utils.creditConversionUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
@@ -21,32 +23,23 @@ public class Scoreboard implements AssembleAdapter {
     public List<String> getLines(Player player) {
         List<String> toReturn = new ArrayList<>();
 
-        int credits = Main.getInstance().economyManager.getCredits(player.getUniqueId());
-        int killstreak = Main.getInstance().profileManager.getProfile(player.getUniqueId()).getKillstreak();
-        int highestKillstreak = Main.getInstance().profileManager.getProfile(player.getUniqueId()).getHighestKillstreak();
-
+        // Check if the player is in Sumo mode
         if (Main.getInstance().sumoManager.isParticipant(player)) {
             toReturn.add(CC.translate(""));
             toReturn.add(CC.translate("&b⤷ Status: &f" + "Waiting"));
             toReturn.add(CC.translate(""));
             toReturn.add(CC.translate("    &7www.senaticpvp.net"));
+            return toReturn;  // Return early for sumo participant
         } else if (Main.getInstance().sumoManager.isFightPair(player)) {
             toReturn.add(CC.translate(""));
             toReturn.add(CC.translate("&b⤷ Status: &f" + "Fighting"));
             toReturn.add(CC.translate(""));
             toReturn.add(CC.translate("    &7www.senaticpvp.net"));
+            return toReturn;  // Return early for sumo fight pair
+        }
 
-        } else if (Main.getInstance().combatManager.isCombat(player)) {
-            toReturn.add(CC.translate(""));
-            toReturn.add(CC.translate("&b⤷ Credits: &f" + credits));
-            toReturn.add(CC.translate("&b⤷ Kills: &f" + player.getStatistic(Statistic.PLAYER_KILLS)));
-            toReturn.add(CC.translate("&b⤷ Killstreak: &f" + killstreak));
-            toReturn.add(CC.translate("&b⤷ Highest Killstreak: &f" + highestKillstreak));
-            toReturn.add(CC.translate("&b⤷ Deaths: &f" + player.getStatistic(Statistic.DEATHS)));
-            toReturn.add(CC.translate("&c⤷ Combat-tagged: &e" + Main.getInstance().combatManager.getCombatTime(player)) + "s");
-            toReturn.add(CC.translate(""));
-            toReturn.add(CC.translate("    &7www.senaticpvp.net"));
-        } else if (Main.getInstance().staffManager.isInStaffMode(player)) {
+        // Check if the player is in Staff mode
+        if (Main.getInstance().staffManager.isInStaffMode(player)) {
             toReturn.add(CC.translate(""));
             toReturn.add(CC.translate("&b⤷ Staff Mode"));
             toReturn.add(CC.translate("&b⤷ Vanish: &f" + (Main.getInstance().staffManager.isVanished(player) ? "&aTrue" : "&cFalse")));
@@ -54,16 +47,42 @@ public class Scoreboard implements AssembleAdapter {
             toReturn.add(CC.translate("&b⤷ RAM: &f" + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1048576 + "MB"));
             toReturn.add(CC.translate(""));
             toReturn.add(CC.translate("    &7www.senaticpvp.net"));
-        } else {
-            toReturn.add(CC.translate("                          "));
-            toReturn.add(CC.translate("&b⤷ Credits: &f" + credits));
-            toReturn.add(CC.translate("&b⤷ Kills: &f" + player.getStatistic(Statistic.PLAYER_KILLS)));
-            toReturn.add(CC.translate("&b⤷ Killstreak: &f" + killstreak));
-            toReturn.add(CC.translate("&b⤷ Highest Killstreak: &f" + highestKillstreak));
-            toReturn.add(CC.translate("&b⤷ Deaths: &f" + player.getStatistic(Statistic.DEATHS)));
-            toReturn.add(CC.translate(""));
-            toReturn.add(CC.translate("    &7www.senaticpvp.net"));
+            return toReturn;  // Return early for staff mode
         }
+
+        // General Info (only for players not in staff mode or sumo)
+        int credits = Main.getInstance().economyManager.getCredits(player.getUniqueId());
+        String creditsDisplay = creditConversionUtil.formatNumber(credits);
+
+        int killstreak = Main.getInstance().profileManager.getProfile(player.getUniqueId()).getKillstreak();
+        int highestKillstreak = Main.getInstance().profileManager.getProfile(player.getUniqueId()).getHighestKillstreak();
+
+        toReturn.add(CC.translate(""));
+        toReturn.add(CC.translate("&b⤷ Credits: &f" + creditsDisplay));
+        toReturn.add(CC.translate("&b⤷ Kills: &f" + player.getStatistic(Statistic.PLAYER_KILLS)));
+        toReturn.add(CC.translate("&b⤷ Killstreak: &f" + killstreak));
+        toReturn.add(CC.translate("&b⤷ Highest Killstreak: &f" + highestKillstreak));
+        toReturn.add(CC.translate("&b⤷ Deaths: &f" + player.getStatistic(Statistic.DEATHS)));
+
+        // Combat status
+        if (Main.getInstance().combatManager.isCombat(player)) {
+            toReturn.add(CC.translate("&c⤷ Combat-tagged: &e" + Main.getInstance().combatManager.getCombatTime(player)) + "s");
+        }
+
+        // Cooldown status (append only if cooldowns are active)
+        if (Main.getInstance().cooldownManager.isOnCooldown(player, CooldownManager.CooldownType.NOTCH_APPLE)) {
+            int godAppleTime = Main.getInstance().cooldownManager.getCooldownTime(player, CooldownManager.CooldownType.NOTCH_APPLE);
+            toReturn.add(CC.translate("&b⤷ &eGod Apple: &f" + godAppleTime + "s"));
+        }
+
+        if (Main.getInstance().cooldownManager.isOnCooldown(player, CooldownManager.CooldownType.ENDERPEARL)) {
+            int pearlCooldownTime = Main.getInstance().cooldownManager.getCooldownTime(player, CooldownManager.CooldownType.ENDERPEARL);
+            toReturn.add(CC.translate("&b⤷ &3Enderpearl: &f" + pearlCooldownTime + "s"));
+        }
+
+        // Footer
+        toReturn.add(CC.translate(""));
+        toReturn.add(CC.translate("    &7www.senaticpvp.net"));
 
         return toReturn;
     }
